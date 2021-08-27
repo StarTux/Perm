abaf499fa1079f66f6fa9f36e0a1fb39a41d5781
StarTux <ths.loy@googlemail.com>
2021-08-28 01:11:57 +0200

    Improve promote command

--- src/main/java/com/winthier/perm/PromoteCommand.java
+++ src/main/java/com/winthier/perm/PromoteCommand.java
@@ -1,56 +1,86 @@
 package com.winthier.perm;
 
 import com.winthier.playercache.PlayerCache;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
-import java.util.UUID;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabExecutor;
 
 @RequiredArgsConstructor
-public final class PromoteCommand implements CommandExecutor {
+public final class PromoteCommand implements TabExecutor {
     private final PermPlugin plugin;
+    final List<String> ranks = Arrays.asList("friendly", "member", "iron"); // iron is temporary
 
     @Override
     public boolean onCommand(final CommandSender sender, final Command command, final String label, final String[] args) {
-        if (args.length != 1) return false;
-        String playerName = args[0];
-        UUID playerUuid = PlayerCache.uuidForName(playerName);
-        if (playerUuid == null) {
-            sender.sendMessage("Player not found: " + playerName);
+        if (args.length != 1 && args.length != 2) return false;
+        PlayerCache player = PlayerCache.forName(args[0]);
+        if (player == null) {
+            sender.sendMessage(Component.text("Player not found: " + args[0], NamedTextColor.RED));
             return true;
         }
-        playerName = PlayerCache.nameForUuid(playerUuid);
-        String fromGroupArg = "friendly";
-        String toGroupArg = "member";
+        final String fromGroupArg;
+        final String toGroupArg;
+        if (args.length == 2) {
+            toGroupArg = args[1];
+            int index = ranks.indexOf(args[1]);
+            if (index < 0) {
+                sender.sendMessage(Component.text("Rank not found: " + toGroupArg, NamedTextColor.RED));
+                return true;
+            }
+            if (index == 0) {
+                sender.sendMessage(Component.text("Cannot promote to: " + toGroupArg, NamedTextColor.RED));
+                return true;
+            }
+            fromGroupArg = ranks.get(index - 1);
+        } else {
+            fromGroupArg = "friendly";
+            toGroupArg = "member";
+        }
         SQLGroup fromGroup = plugin.cache.findGroup(fromGroupArg);
         if (fromGroup == null) {
-            sender.sendMessage("Group not found: " + fromGroupArg);
+            sender.sendMessage(Component.text("Group not found: " + fromGroupArg, NamedTextColor.RED));
             return true;
         }
         SQLGroup toGroup = plugin.cache.findGroup(toGroupArg);
         if (toGroup == null) {
-            sender.sendMessage("Group not found: " + toGroupArg);
+            sender.sendMessage(Component.text("Group not found: " + toGroupArg, NamedTextColor.RED));
             return true;
         }
-        List<String> groups = plugin.findPlayerGroups(playerUuid);
+        List<String> groups = plugin.findPlayerGroups(player.uuid);
         if (!groups.contains(fromGroup.getKey())) {
-            sender.sendMessage(playerName + " not in group " + fromGroup.getDisplayName());
+            sender.sendMessage(Component.text(player.name + " not in group " + fromGroup.getDisplayName(), NamedTextColor.RED));
             return true;
         }
         if (groups.contains(toGroup.getKey())) {
-            sender.sendMessage(playerName + " already in group " + toGroup.getDisplayName());
+            sender.sendMessage(Component.text(player.name + " already in group " + toGroup.getDisplayName(), NamedTextColor.RED));
             return true;
         }
         plugin.db.find(SQLMember.class)
-            .eq("member", playerUuid)
+            .eq("member", player.uuid)
             .eq("group", fromGroup.getKey())
             .delete();
-        plugin.db.insert(new SQLMember(playerUuid, toGroup.getKey()));
+        plugin.db.insert(new SQLMember(player.uuid, toGroup.getKey()));
         plugin.updateVersion();
         plugin.refreshPermissions();
-        sender.sendMessage(playerName + " promoted to " + toGroup.getDisplayName());
+        sender.sendMessage(Component.text(player.name + " promoted to " + toGroup.getDisplayName(), NamedTextColor.YELLOW));
         return true;
+    }
+
+    @Override
+    public List<String> onTabComplete(final CommandSender sender, final Command command, final String label, final String[] args) {
+        if (args.length == 1) return null;
+        if (args.length == 2) {
+            return ranks.subList(1, ranks.size()).stream()
+                .filter(a -> a.contains(args[1]))
+                .collect(Collectors.toList());
+        }
+        return Collections.emptyList();
     }
 }
